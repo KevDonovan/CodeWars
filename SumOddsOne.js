8bead7541a3816c142b005847a42127e25639224
Kevin Donovan <kevdonovan88@gmail.com>
2023-03-16 21:32:50 -0500

    3-16-23

---
+++ SumOddsOne.js
@@ -0,0 +1,12 @@
+function rowSumOddNumbers(n) {
+    let sum = 0;
+    let rowStart = 1;
+    
+    for(let i = 0; i < n; i++) {
+      rowStart += i*2;
+    }
+    for( let i = 0; i < n; i++){
+      sum += rowStart + i*2;
+    } 
+    return sum;
+  }
